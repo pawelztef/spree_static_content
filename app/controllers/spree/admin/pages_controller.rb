51fcb6d7cb8e61b07e9b920493f3145c3642a437
pawelztef <pawelztef@gmail.com>
2018-02-04 10:01:43 +0000

    helper added to views, views udpated

--- app/controllers/spree/admin/pages_controller.rb
+++ app/controllers/spree/admin/pages_controller.rb
@@ -1,10 +1,54 @@
 module Spree
   module Admin
     class PagesController < ResourceController
+      helper 'spree/admin/pages'
+      before_action :set_static_page_mobility
+
+      def index
+        byebug
+        @pages = Spree::Page.all
+      end
+
+      def edit
+        @page = Spree::Page.find(params[:id])
+      end
+
+      def update
+        @page = Spree::Page.find(params[:id])
+        if @page.update(page_params)
+          flash[:success]=flash_message_for(@page, :successfully_updated)
+          redirect_to admin_pages_url
+        else
+          render :new
+        end
+      end
+
+      def create
+        @page = Spree::Page.new(page_params)
+        if @page.save
+          flash[:success] = flash_message_for(@page, :successfully_created)
+          redirect_to admin_pages_url
+        else
+          flash[:error] = flash_message_for(@page, :not_created)
+          render :new
+        end
+      end
+
+      def mobility
+        session[:static_page_mobility_locale] = params[:mobility_locale]
+        redirect_to admin_pages_url
+      end
 
       private
+
       def page_params
         params.require(:page).premit!
+      end
+
+      def set_static_page_mobility
+        if session[:static_page_mobility_locale]
+          Mobility.locale = session[:static_page_mobility_locale]
+        end
       end
     end
   end
--- app/helpers/spree/pages_helper.rb
+++ app/helpers/spree/pages_helper.rb
@@ -4,5 +4,21 @@ module Spree
       page = Spree::Page.find_by(slug: slug)
       raw page.body if page
     end
+
+    def set_locales_array
+      begin
+        loc = SpreeI18n::Config.available_locales  
+      rescue  
+        loc = ['en']
+      end 
+    end 
+
+    def show_visibility entry 
+      if entry.visible
+       content_tag(:i, '', :class => 'glyphicon glyphicon-check')
+      else
+       content_tag(:i, '', :class => 'glyphicon glyphicon-unchecked')
+      end
+    end
   end
 end
